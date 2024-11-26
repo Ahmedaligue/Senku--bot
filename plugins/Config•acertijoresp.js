06f6207d5ea069ecb91c5839fbd8cc480e28f707
Ahmedaligue <158603161+Ahmedaligue@users.noreply.github.com>
2024-11-26 21:43:46 +0100

    Update Config•acertijoresp.js

--- plugins/Config•acertijoresp.js
+++ plugins/Config•acertijoresp.js
@@ -5,16 +5,16 @@ handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('✨️ Ese acertijo ya ha terminado!');
+  if (!(id in this.tekateki)) return m.reply('✨️ !');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
       global.db.data.users[m.sender].estrellas += this.tekateki[id][2];
-      m.reply(`🌵 *Respuesta correcta!*\n+${this.tekateki[id][2]} Galletas`);
+      m.reply(`🌵 *اجابة صحيحة!*\n+${this.tekateki[id][2]} هده بعض العملات`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
     } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`🚩 Casi lo logras!`);
-    else m.reply('🍭 Respuesta incorrecta!');
+    else m.reply('*اجابة خاطئة*');
   }
   return !0;
 };
